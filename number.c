02dd4ee5b681f4302b870c0d1cd5038099f2b236
bhuvana12345 <40120511+bhuvana12345@users.noreply.github.com>
2019-05-22 12:10:36 +0530

    void main()

---
+++ number.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int a;
+if (a>0)
+  {
+     printf("positive");
+  }  
+    if(a<0)
+   {
+      printf("negative");
+    }
+      else 
+   {
+      Print("zero")
+   }
+getch();
+}
