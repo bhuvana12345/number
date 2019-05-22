1fed60d672897b8f50fa6989c99903a80375b62a
bhuvana12345 <40120511+bhuvana12345@users.noreply.github.com>
2019-05-22 12:35:31 +0530

    num.c

--- number.c
+++ number.c
@@ -6,15 +6,15 @@ printf("enter a value");
 if (a>0)
 { 
   {
-     printf("positive");
+     printf("Positive");
   }  
   else if(a<0)
    {
-      printf("negative");
+      printf("Negative");
     }
       else 
         {
-            Print("zero")
+            Print("Zero")
         }
 } 
 getch();
